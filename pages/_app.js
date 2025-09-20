33edd8142daa89e3c34404982ce99bebd06fb933
BillyOHightouch <billy@hightouch.io>
2025-09-19 18:45:29 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,7 @@
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// pages/_app.tsx or pages/_app.js
+import '@/styles/globals.css'; // or '../styles/globals.css' if you don't have a tsconfig path alias
+
+import type { AppProps } from 'next/app';
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
